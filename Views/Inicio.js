cbb62b15c1d96243a6745f9973eff89759a9ffec
LetcAndrade <lcarneiroandrade3@gmail.com>
2022-12-04 22:13:19 -0300

    subindo código

--- Views/Inicio.js
+++ Views/Inicio.js
@@ -46,34 +46,35 @@ export default function Principal({ navigation })
             'Content-Type': 'application/json'
           },
           });
-          jsonResponse = await response.json();
+
+          let jsonResponse = await response.json();
           console.log(jsonResponse)
-          setJsonResponse(jsonResponse)
+          setJsonResponse(jsonResponse.totalDoado)
         }
         getUser();
         getTsangue();
         getId();
         getJsonResponse();
+
+
     },[]);
 
    
   
   return (
     <View style ={styles.container}>
-      <View style = {styles.container2}>
-       <Text style={styles.Perg}>Olá, {user}!</Text>
-       <Image source={require('../assets/banco.png')}  
-                style={{ resizeMode: "contain", height: 200, width: 400}}/>
-       </View>
+       <Text style={styles.ajud}>Olá, {user}!</Text>
        <Text style={styles.Sangue}>{tsangue}</Text>
+       <Text style = {styles.ajud}>♥Total de pessoas ajudadas: 0</Text>
        <Image source={require('../assets/principal.png')}  
-                style={{ resizeMode: "contain", height: 200, width: 400}}/>
-       <Text style = {styles.ajud}>Total de pessoas ajudadas: {jsonResponse}</Text>
-       <Text style = {styles.ajud}>Total de doação realizada: {jsonResponse}</Text>
+                style={{ resizeMode: "contain", height: 300, width: 400}}/>
+       <Text style = {styles.ajud}>♥Total de doação realizada: 0 </Text>
+       <View style = {styles.container3}></View>
+       <Text style = {styles.ajud2}>♥Sua ajuda faz toda a diferença! Doe Sangue, Doe Vida!♥</Text>
      <View style={styles.containerPos}>
             <TouchableOpacity style={styles.button__home} onPress={() => navigation.navigate('Inf')}>
                 <Image source={require('../assets/inf.png')}  
-                style={{ resizeMode: "contain", height: 100, width: 70}}/>
+                style={{ resizeMode: "contain", height: 100, width: 90}}/>
             </TouchableOpacity>
             <TouchableOpacity style={styles.button__home} onPress={() => navigation.navigate('Agend')}>
                 <Image source={require('../assets/agend.png')}
